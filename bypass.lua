1453358374b1722d904155ad11065b0c71a7aac7
barbarianTopG <134774093+barbarianTopG@users.noreply.github.com>
2025-09-14 18:49:51 +1000

    Update bypass.lua

--- bypass.lua
+++ bypass.lua
@@ -1,1 +1,1 @@
-for i,v in getnilinstances() do pcall(function() if v:IsA("BaseScript") then v:Destroy() end end) end
+for i,v in getnilinstances() do pcall(function() if v:IsA("BaseScript") then for k,j in getconnections(v.Changed) do v:Disconnect() end v:Destroy() end end) end
